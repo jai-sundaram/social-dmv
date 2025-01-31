6765e998e1d01fb7ff5671d75a240bf6de2dc3f0
jaivatsan-elango-sundaram <je.sundaram@gmail.com>
2025-01-30 22:16:24 -0500

    began working on the home page

---
+++ social-dmv-project/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage: {
+        bannerImg: "url('/dc-night.jpg')"
+      }
+    },
+  },
+  variants: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
