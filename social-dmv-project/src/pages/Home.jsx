a80cd51a7fab3e76e795e5127c1f10cc1c6673fa
jaivatsan-elango-sundaram <je.sundaram@gmail.com>
2025-02-04 15:21:15 -0500

    tried playing around with it

--- social-dmv-project/src/pages/Home.jsx
+++ social-dmv-project/src/pages/Home.jsx
@@ -1,28 +1,34 @@
 import React from 'react'
-import {Link} from "react-router-dom";
+import {Link} from 'react-scroll'
 import Info from "./Info.jsx";
 
 const Home = () => {
     return (
         <div className="bg-black w-full h-full">
-            <div className="fixed top-0 right-125">
+            <div className="absolute top-0 right-125">
                 <img className="" src="logo5.png"/>
             </div>
-            <div className="fixed top-80 right-100">
+            <div className="absolute top-80 right-100" id ="home_tag">
                 <h1 className="text-gray-50 font-impact text-9xl">SOCIAL DMV</h1>
             </div>
-            <div className="fixed top-120 right-95">
+            <div className="absolute top-120 right-95">
                 <h1 className="text-gray-50 font-bebas-neue ">Explore the best restaurants, bars, and clubs and much
                     more in the Washington D.C. area</h1>
             </div>
 
-            <div className="fixed top-135 right-143">
-                <a href="/Info">
+            <div className="absolute top-135 right-143">
+                <Link
+                    to="info_section"
+                    spy={true}
+                    smooth={true}
+                    offset={50}
+                    duration={500}
+                >
                     <button
                         className="h-20 w-62  bg-gradient-to-r from-purple-500 to-pink-500 hover: shadow-lg hover:shadow-fuchsia-300 hover:scale-120 text-white font-bold ">
                         Get Started
                     </button>
-                </a>
+                </Link>
             </div>
 
         </div>
--- social-dmv-project/src/pages/Info.jsx
+++ social-dmv-project/src/pages/Info.jsx
@@ -1,55 +1,17 @@
-import React from 'react'
-import Home from './Home.jsx'
 const Info = () => {
     return (
-        <div>
-            <div className="fixed top-0 right-100">
-                <h1 className="text-gray-50 font-impact text-9xl">SOCIAL DMV</h1>
+        <div id="info_section" className="w-full h-screen">
+            <div className="absolute top-0 right-100">
+                <h1 className="text-gray-50 font-impact text-9xl" id="info_tag">SOCIAL DMV</h1>
             </div>
-            <div className="fixed top-70 right-0">
+            <div className="absolute top-70 right-0">
                 <p className="text-gray-50">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab cum dicta dolore eum expedita
-                    explicabo, incidunt ipsam laudantium libero, molestias natus necessitatibus obcaecati officiis
-                    perspiciatis porro quae saepe temporibus veniam?
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit...
                 </p>
             </div>
-            <div className="fixed top-110 right-0">
-                <p className="text-gray-50">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab cum dicta dolore eum expedita
-                    explicabo, incidunt ipsam laudantium libero, molestias natus necessitatibus obcaecati officiis
-                    perspiciatis porro quae saepe temporibus veniam?
-                </p>
-            </div>
-            <div className="fixed top-110 right-0">
-                <p className="text-gray-50">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab cum dicta dolore eum expedita
-                    explicabo, incidunt ipsam laudantium libero, molestias natus necessitatibus obcaecati officiis
-                    perspiciatis porro quae saepe temporibus veniam?
-                </p>
-            </div>
-            <div className="fixed top-110 right-0">
-                <p className="text-gray-50">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab cum dicta dolore eum expedita
-                    explicabo, incidunt ipsam laudantium libero, molestias natus necessitatibus obcaecati officiis
-                    perspiciatis porro quae saepe temporibus veniam?
-                </p>
-            </div>
-            <div className="fixed top-130 right-0">
-                <p className="text-gray-50">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab cum dicta dolore eum expedita
-                    explicabo, incidunt ipsam laudantium libero, molestias natus necessitatibus obcaecati officiis
-                    perspiciatis porro quae saepe temporibus veniam?
-                </p>
-            </div>
-            <div className="fixed top-150 right-0">
-                <p className="text-gray-50">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab cum dicta dolore eum expedita
-                    explicabo, incidunt ipsam laudantium libero, molestias natus necessitatibus obcaecati officiis
-                    perspiciatis porro quae saepe temporibus veniam?
-                </p>
-            </div>
+            {/* Rest of your Info content */}
         </div>
+    )
+}
 
-        )
-        }
-        export default Info
+export default Info
