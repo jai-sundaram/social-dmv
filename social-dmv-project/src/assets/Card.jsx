07a67da837f4746b318b398d59367374e2625cc7
jaivatsan-elango-sundaram <je.sundaram@gmail.com>
2025-03-02 01:45:40 -0500

    added editorial summary

--- social-dmv-project/src/assets/Card.jsx
+++ social-dmv-project/src/assets/Card.jsx
@@ -1,18 +1,14 @@
 import React, {useEffect} from 'react'
 const GOOGLE_KEY = import.meta.env.VITE_GOOGLE_KEY
 
-/*
-        <p key = {place.id} className="text-gray-50 font-impact relative left-150 top-50">
-            {place.displayName.text}
-        </p>
- */
+
 const Card = ({place}) => {
+    let price;
     return (
             <div className = "m-4 flex-none w-[calc(33.33%-20px)] max-w-[calc(33.33%-20px)]">
-            <div className = " text-xl rounded-2xl w-full h-120 overflow-hidden shadow-xl border-4 border-fuchsia-300 shadow-white">
-                <h1 className = "font-bold text-gray-50 justify-self-center  mt-10"> {place.displayName.text}</h1>
-                <p className ="text-gray-50">More info</p>
-                <p className ="text-gray-50">More info</p>
+            <div className = " text-2xl rounded-2xl w-full h-120 overflow-hidden shadow-xl border-4 border-fuchsia-300 shadow-white">
+                <p className ="font-bold text-gray-50 justify-self-center  mt-10">{place.displayName.text}</p>
+                <p className ="font-bold text-gray-50 justify-self-center  mt-2 ml-2 mr-2 font-card-name text-lg">{place.editorialSummary?.text || 'No summary available'}</p>
                 <p className ="text-gray-50">More info</p>
             </div>
             </div>
